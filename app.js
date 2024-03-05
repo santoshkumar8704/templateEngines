aac99902f95ebafff6479403eca68136362579bf
YERUBANDI SATYA SANTOSH KUMAR <132978331+santoshkumar8704@users.noreply.github.com>
2024-03-05 15:06:17 +0530

    pug

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser')
 
-const app = express()
-app.get("/",(req,res,next) => {
-    res.render()
-})
-app.get("/users",(req,res,next) => {
-    res.render()
-})
-app.post("/adduser",(req,res,next) => {
-    res.redirect("/")
-})
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', 'views');
+const users = []
+
+app.use(bodyParser.urlencoded({extended:false}))
 
 
 
-app.listen('3000')
+
+app.get("/", (req, res, next) => {
+    res.render("index", { pageTitle: "add user" });
+});
+
+app.get("/users", (req, res, next) => {
+    res.render("users", { pageTitle: "users" ,users:users});
+});
+
+app.post("/adduser", (req, res, next) => {
+    users.push({name: req.body.username})
+    res.redirect("/");
+});
+
+app.listen(3000);
