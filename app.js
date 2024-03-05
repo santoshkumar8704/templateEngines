54dc276d41b237c87279c363382ae1545ef5a133
YERUBANDI SATYA SANTOSH KUMAR <132978331+santoshkumar8704@users.noreply.github.com>
2024-03-05 14:43:54 +0530

    commit 1

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 const express = require('express')
-const app = express()
 
+const app = express()
+app.get("/",(req,res,next) => {
+    res.render()
+})
+app.get("/users",(req,res,next) => {
+    res.render()
+})
+app.post("/adduser",(req,res,next) => {
+    res.redirect("/")
+})
+
+
+
+app.listen('3000')
